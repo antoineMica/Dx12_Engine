985ec9c1c2b4036f0c4caf514167ed3dd793131a
Toto <tottz01@gmail.com>
2017-06-30 00:51:03 -0700

    Added empty Renderer

---
+++ Dx12_Engine/DX12Renderer.cpp
@@ -0,0 +1,12 @@
+#include "DX12Renderer.h"
+
+
+
+DX12Renderer::DX12Renderer()
+{
+}
+
+
+DX12Renderer::~DX12Renderer()
+{
+}
---
+++ Dx12_Engine/DX12Renderer.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <d3d12.h>  //directx 12 include
+
+
+class DX12Renderer
+{
+public:
+	DX12Renderer();
+	~DX12Renderer();
+};
+
